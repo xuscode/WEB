f70a32ea3a8d6cfe516f31b64558a82968b2f720
song_hsu <song_hsu@163.com>
2023-10-30 12:10:47 +0800

    delete

--- DS-CATIA/001.bat
+++
@@ -1,9 +0,0 @@
-
-@echo off
-echo Input you Project_Name.
-set /p Project_Name=
-echo Your Project_Name is %Project_Name%.
-
-
-md %Project_Name%
-echo # %Project_Name% >%Project_Name%.md
--- DS-CATIA/DSLS/set-ds-license.bat
+++
@@ -1,4 +0,0 @@
-
-md C:\ProgramData\DassaultSystemes\Licenses\
-echo l2.ttut.cc:50091 >C:\ProgramData\DassaultSystemes\Licenses\DSLicSrv.txt
-
